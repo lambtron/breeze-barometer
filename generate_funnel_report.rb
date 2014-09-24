22716d46c50b59224f3430a910454408c17b6e9c
Andy Jiang <andyjiang@gmail.com>
2014-09-23 20:11:58 -0700

    update

---
+++ generate_funnel_report.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'json'
+require 'pp'
+
+file = ARGV[0]
+
+f = File.open(file, "r")
+
+arr = ''
+
+f.each_line do |line|
+  arr = JSON.parse(line)
+  funnel = {}
+  arr.each do |user|
+    neighborhood = user['neighborhood']
+    unless neighborhood.nil?
+      neighborhood = neighborhood.gsub(/[^0-9a-z ]/i, '').tr(" ", "_")
+    else
+      neighborhood = 'NA'
+    end
+    current_stage = user['current_stage']
+
+    if funnel.key?(neighborhood)
+      if funnel[neighborhood].key?(current_stage)
+        funnel[neighborhood][current_stage] += 1
+      else
+        funnel[neighborhood][current_stage] = 0
+      end
+    else
+      funnel[neighborhood] = {}
+    end
+  end
+
+  pp funnel
+end
